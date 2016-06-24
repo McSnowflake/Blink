7654ae4c24643a20fde4e19fd7099992719f103d
Martin <m_weise@gmx.net>
2016-06-24 23:32:23 +0200

    bla

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,40 @@
 /**
 Author: Martin Weise
 **/
-
+#include "Wire.h"
 #include <Arduino.h>
 //#include <vector>
-
-const auto d = 0.0;
-const byte ledPin = 4;
-const byte interruptPin = 3;
-volatile byte state = LOW;
+const int aNumbers[]= {1,2,3,4,5};
+const char* pcPlainOldString ="Hello World";
+const auto dVar = 0.0;
+const byte bLedPin = 4;
+const byte bInterruptPin = 3;
+volatile byte bState = LOW;
 
 void blink();
 
 void setup() {
   Serial.begin(9600);
     Serial.println("start");
-  pinMode(ledPin, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+  pinMode(bLedPin, OUTPUT);
+  pinMode(bInterruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(bInterruptPin), blink, CHANGE);
 }
 
 void loop() {
-  Serial.println(state);
-  digitalWrite(ledPin, state);
+  Serial.println(bState);
+  for(const auto& item : aNumbers)
+  {
+    Serial.println(pcPlainOldString);
+    Serial.println(item);
+  }
+  digitalWrite(bLedPin, bState);
   delay(1000);
 }
 
 void blink() {
   Serial.println("IRQ");
-  state = !state;
+  bState = !bState;
 }
 /**int main () {
   Serial.println("start");
