e33fea17feffd421e8f0456ac31ebdc9613facae
Franziskus <franziskuswild@aim.com>
2016-06-24 23:36:40 +0200

    display scheiße

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,22 @@ Author: Martin Weise
 **/
 
 #include <Arduino.h>
-#include <LiquidCrystal.h>
 #include "TimerOne.h"
-
+#include "Wire.h"
+#include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-const auto ledPin = 4;
-const byte interruptPin = 3;
 volatile byte state = LOW;
 
 void blink();
 
 void setup() {
-  Serial.begin(9600);
-    Serial.println("start");
-  pinMode(ledPin, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+  lcd.begin(16, 2);
+  //Serial.begin(9600);
+    //Serial.println("start");
+    lcd.setCursor(5, 0);
+    lcd.print("init");
+    lcd.setCursor(0, 1);
 
 Timer1.initialize(500000);         // initialize timer1, and set a 1/2 second period
 //Timer1.pwm(9, 512);                // setup pwm on pin 9, 50% duty cycle
@@ -28,20 +27,22 @@ Timer1.attachInterrupt(blink);  // attaches callback() as a timer overflow inter
 }
 
 void loop() {
-  Serial.println(state);
-  digitalWrite(ledPin, state);
-  delay(100);
+  if (state == LOW) {
+     lcd.print("IRQ");
+  } else {
+     lcd.print("     ");
+  }
+  lcd.setCursor(5, 1);
+  lcd.print(state);
+  lcd.setCursor(0, 1);
+
+
+    delay(100);
 }
 
 void blink() {
-  Serial.println("IRQ");
-  state = !state;
+  //Serial.println("IRQ");
+
+
+state = !state;
 }
-/**int main () {
-  Serial.println("start");
-  setup();
-
-  while(true)
-    loop();
-
-}**/
