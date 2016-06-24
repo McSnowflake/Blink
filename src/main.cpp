2f05bce465a95385d6ed6a566cd460228b8249da
Franziskus <franziskuswild@aim.com>
2016-06-24 21:04:21 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,34 @@
 #include <Arduino.h>
 
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+const byte ledPin = 4;
+const byte interruptPin = 3;
+volatile byte state = LOW;
 
-  This example code is in the public domain.
- */
+void blink();
 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 4;
-
-// the setup routine runs once when you press reset:
 void setup() {
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
   Serial.begin(9600);
+    Serial.println("start");
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.println("on");           // wait for a second
+  Serial.println(state);
+  digitalWrite(ledPin, state);
   delay(1000);
-  Serial.println("off");           // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
 }
 
-int main () {
-
-setup();
-
-while(true)
-  loop();
-
+void blink() {
+  Serial.println("IRQ");
+  state = !state;
 }
+/**int main () {
+  Serial.println("start");
+  setup();
+
+  while(true)
+    loop();
+
+}**/
